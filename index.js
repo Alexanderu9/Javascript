5cf4cfef29bd45e7bfc7e993268042b2ee98abdc
Alexanderu9 <alexanderu7.aug@gmail.com>
2025-03-24 20:35:42 -0300

    Ex 3 - If/Else: Salário por hora
    Agora, vamos aumentar a complexidade do nosso código.
    
    Por legislação, temos a questão de horas extras: todos os funcionários trabalham, mensalmente, 220 horas. Quando o valor de horas fica acima de 220, temos de considerar que as horas excedentes são recebidas dobradas, ou seja, um funcionário que trabalhou 230 horas receberá 220 horas normais e 10 horas extras (com valor dobrado).
    
    Utilize o if/else e seu conhecimento matemático para fazer o cálculo do salário, levando essa regra em consideração.

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-let horasTrabaladas = 220;
+let horasTrabaladas = 230;
 let valorDaHora = 11.75;
-let salarioPorHora = horasTrabaladas * valorDaHora;
 let nomeDoFuncionario = "Alexander";
 let setor = "Desenvolvimento";
-console.log("O salario de " + nomeDoFuncionario + " no setor " + setor + " é " + salarioPorHora + " reais ao mes.");
+
+let salarioPorHora = horasTrabaladas * valorDaHora;
+
+if (horasTrabaladas > 220) {
+let horaExcedente = horasTrabaladas - 220;
+let valorExcedente = valorDaHora * 2;
+let adicionalExcedente = horaExcedente * valorExcedente;
+let salarioTotal = (220 * valorDaHora) + adicionalExcedente;
+console.log(`O salário de ${nomeDoFuncionario} no setor ${setor} é de ${salarioTotal} reais por mes`);
+}
+
+else {
+ console.log(`O salário de ${nomeDoFuncionario} no setor ${setor} é de ${salarioPorHora} reais por mes`);
+}
+
