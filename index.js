148fad83a1330e918ed279bf00d5332ba9caf3ad
Alexanderu9 <alexanderu7.aug@gmail.com>
2025-03-24 19:30:33 -0300

    Ex 2 - Operadores Aritméticos: Salário por hora
    Com nossas variáveis criadas, podemos fazer o cálculo base para termos o salário por hora.
    
    Esse cálculo é a multiplicação entre o número de horas trabalhadas e o valor do salário por hora.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let horasTrabaladas = 220;
 let valorDaHora = 11.75;
+let salarioPorHora = horasTrabaladas * valorDaHora;
 let nomeDoFuncionario = "Alexander";
 let setor = "Desenvolvimento";
+console.log("O salario de " + nomeDoFuncionario + " no setor " + setor + " é " + salarioPorHora + " reais ao mes.");
