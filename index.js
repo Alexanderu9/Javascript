3686f9b7659a005aba64f48575d9b9d7cafa15f2
Alexanderu9 <alexanderu7.aug@gmail.com>
2025-03-24 22:02:37 -0300

    Ex 4 - Operadores lógicos: Salário por hora
    Sabemos que também temos a questão dos setores e, levando isso em consideração, aplique as seguintes regras para os setores:
    
    [OP]Operacional: Não é necessário nenhuma mudança de cálculos;
    
    [AD]Administrativo: O valor do salário base é 10% maior;
    
    [GE]Gerência: O valor do salário base é 25% maior, mas não possui horas extras.

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-let horasTrabaladas = 230;
+let horasTrabaladas = 220;
 let valorDaHora = 11.75;
 let nomeDoFuncionario = "Alexander";
-let setor = "Desenvolvimento";
+let setor = "Operacional";
 
 let salarioPorHora = horasTrabaladas * valorDaHora;
 
-if (horasTrabaladas > 220) {
+
+if (setor === "Operacional") {
 let horaExcedente = horasTrabaladas - 220;
 let valorExcedente = valorDaHora * 2;
 let adicionalExcedente = horaExcedente * valorExcedente;
 let salarioTotal = (220 * valorDaHora) + adicionalExcedente;
 console.log(`O salário de ${nomeDoFuncionario} no setor ${setor} é de ${salarioTotal} reais por mes`);
 }
+else if (setor === "Administrativo") {
 
-else {
- console.log(`O salário de ${nomeDoFuncionario} no setor ${setor} é de ${salarioPorHora} reais por mes`);
+let horaExcedente = horasTrabaladas - 220;
+let valorExcedente = valorDaHora * 2;
+let adicionalExcedente = horaExcedente * valorExcedente;
+let salarioComHoras = (220 * valorDaHora) + adicionalExcedente;
+let porcentagemAdicional = (salarioComHoras * 10) / 100;
+let total = salarioComHoras + porcentagemAdicional;
+
+
+console.log(`O salário de ${nomeDoFuncionario} no setor ${setor} é de ${ total} reais por mes`);
 }
 
+else if (setor === "Gerencia") {
+
+let porcentagemAdicional = (salarioPorHora * 25) / 100;
+let total = salarioPorHora + porcentagemAdicional;
+
+
+console.log(`O salário de ${nomeDoFuncionario} no setor ${setor} é de ${ total} reais por mes`);
+}
+
+else {
+
+console.log(`O salário de ${nomeDoFuncionario} no setor ${setor} é de ${salarioPorHora} reais por mes`);
+}
