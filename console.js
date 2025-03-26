1a9d3fa61cf897c2db5ecca293196a2f26684b37
Alexanderu9 <alexanderu7.aug@gmail.com>
2025-03-26 00:29:22 -0300

    Ex 6 - Exibição: Salário por hora
    Mostre no console a seguinte mensagem, levando em consideração as variáveis que colocarei em negrito:
    
    O funcionário, do setor administrativo, João teve 220 horas trabalhadas e 20 horas extras. Seu salário mensal foi de R$ 2.560,00.

---
+++ console.js
@@ -0,0 +1,7 @@
+let funcionario = "João";
+let setor = "Administrativo";
+let horasTrabalhadas = 220;
+let horasExtras = 20;
+let valorDaHora = 10.67;
+let salarioTotal = (horasTrabalhadas + horasExtras) * valorDaHora;
+console.log(`O salário de ${funcionario} no setor ${setor} é de ${salarioTotal} reais por mes`);
