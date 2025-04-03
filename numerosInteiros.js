14b0cc064ede7746a6e85782ecb1095a1e7f8323
Alexanderu9 <alexanderu7.aug@gmail.com>
2025-04-03 03:15:17 -0300

    Ex 2 - Do while/while: Números pares
    Desenvolva uma função onde você receberá um número inteiro e, ela, lhe retornará quantos números pares existem até a chegada do número informado. Utilize a estrutura de repetição DO WHILE/WHILE (exclua o número 0 da conta).
    
    Ex.: Informo 10 e o retorno será 5 (que é a quantidade de números pares (2, 4, 6, 8 e 10).

---
+++ numerosInteiros.js
@@ -0,0 +1,39 @@
+console.log(
+    "\\\\\\\\\\\\\\\\\\\\\\\\\\. \n Digite un Numero \n Para comprobar se é par ou não \n \\\\\\\\\\\\\\\\\\\\\\\\\\."
+  );
+  let num = parseInt(prompt("Digite um numero"));
+
+
+  //função para saver se o numero é par ou não
+  function numerosInteiros(num) {
+    if (num % 2 === 0) return true;
+    else return false;
+  }
+  const resultados = numerosInteiros(num);
+  
+  //função para retornar o valor
+ function numerosP() {
+if (resultados)
+return `O número ${num} é par`;
+return `O número ${num} é impar`;
+ }
+
+ const NumerosInfo = numerosP(resultados);
+ console.log(NumerosInfo);
+
+ //função para contar os numeros pares do 1 ao numero digitado
+
+ function ContarNumerosPares(limite) {
+let quantidade= 0;
+let i= 1;
+
+while (i <= limite) {
+    quantidade++;
+    i += 2;
+}
+return quantidade;
+
+ }
+
+ console.log(
+    `Quantidade de números pares de 1 até ${num}: ${ContarNumerosPares(num)}`)
